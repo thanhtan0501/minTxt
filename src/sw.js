d788324332d6bcdc8f9afccb2caf5acb50f42c86
nghkhoi <43063763+nghkhoi@users.noreply.github.com>
2021-03-10 12:52:37 +0700

    pwa offline (#4)
    * Updated service worker with basic offline caching
    
    * Fix service worker caching to support share_target

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,34 @@
-// Mock service worker
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+//Basic asset-caching
+var CACHE='mintxt-cache';
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll([
+        '/mintxt/',
+        '/mintxt/index.html',
+        '/mintxt/script.js',
+        '/mintxt/style.css',
+        '/mintxt/app.webmanifest',
+        '/mintxt/icons/android-chrome-192x192.png',
+        '/mintxt/icons/android-chrome-512x512.png',
+        '/mintxt/icons/apple-touch-icon.png',
+        '/mintxt/icons/favicon-16x16.png',
+        '/mintxt/icons/favicon-32x32.png',
+        '/mintxt/icons/favicon.ico'
+      ]);
+    })
+  );
+ });
+ 
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fromCache(e.request));
 });
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request, { ignoreSearch: true }).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
